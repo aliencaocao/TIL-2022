563a6d6ef15cbbda47852b83607e59ed4e61f088
Ho Wing Yip <sploooosh884@gmail.com>
2022-06-15 16:49:42 +0800

    Integrate detection functionality into cv_service.py

--- til-final-2022.1.1-lower/stubs/cv_service.py
+++ til-final-2022.1.1-lower/stubs/cv_service.py
@@ -1,18 +1,21 @@
 from typing import List, Any
 from tilsdk.cv.types import *
+from tilsdk.cv import DetectedObject
 import onnxruntime as ort
+from mmdet.apis import init_detector, inference_detector
 
 class CVService:
-    def __init__(self, model_dir):
+    def __init__(self, config_file, checkpoint_file):
         '''
         Parameters
         ----------
-        model_dir : str
-            Path of model file to load.
+        config_file : str
+            Path to mmdet config file.
+        checkpoint_file : str
+            Path to model checkpoint.
         '''
 
-        # TODO: Participant to complete.
-        pass
+        self.model = init_detector(config_file, checkpoint_file, device="cuda:0")
 
     def targets_from_image(self, img) -> List[DetectedObject]:
         '''Process image and return targets.
@@ -28,8 +31,26 @@ class CVService:
             Detected targets.
         '''
         
-        # TODO: Participant to complete.
-        pass
+        result = inference_detector(self.model, img)
+        detections = []
+        
+        current_detection_id = 1
+        for class_id, this_class_detections in enumerate(result):
+            for detection in this_class_detections:
+                x1, y1, x2, y2, _confidence = [float(x) for x in detection]
+                detections.append(DetectedObject(
+                    id=current_detection_id,
+                    cls="fallen" if class_id == 0 else "standing",
+                    bbox={
+                        "x": x1,
+                        "y": y1,
+                        "w": x2 - x1,
+                        "h": y2 - y1,
+                    }
+                ))
+                current_detection_id += 1
+
+        return detections
 
 
 class MockCVService:
